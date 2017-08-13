685ca23d5d8ccff4808a92ac17fcee8558836fd1
matrostik <matrostik@gmail.com>
2017-08-13 10:33:38 +0300

    Thymeleaf template engine

---
+++ src/main/java/sfdc/mc/controller/DemoController.java
@@ -0,0 +1,35 @@
+package sfdc.mc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Calendar;
+
+@Controller
+public class DemoController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @RequestMapping("/test")
+    public String test(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        model.addAttribute("year", year);
+
+        return "bootstrap";
+    }
+
+    @RequestMapping(value = "/lightning")
+    public String lightning() {
+        System.out.println("*******************************");
+
+        return "lightning";
+    }
+
+}
